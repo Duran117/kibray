7d47850aae4f8991cc0c77a552ba33264361c644
Jesus Duran <tucorreo@example.com>
2025-07-29 21:17:16 -0600

    Agrego comando personalizado createsu para crear superusuario

---
+++ core/management/commands/createsu.py
@@ -0,0 +1,13 @@
+from django.contrib.auth import get_user_model
+from django.core.management.base import BaseCommand
+
+class Command(BaseCommand):
+    help = 'Crea un superusuario automáticamente si no existe'
+
+    def handle(self, *args, **options):
+        User = get_user_model()
+        if not User.objects.filter(username='admin').exists():
+            User.objects.create_superuser('admin', 'admin@example.com', 'admin123')
+            self.stdout.write(self.style.SUCCESS('Superusuario creado: admin/admin123'))
+        else:
+            self.stdout.write(self.style.WARNING('El superusuario ya existe'))
